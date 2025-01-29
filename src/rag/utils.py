9d43409f131a32c4744f8af2e589540f356ef11d
BaptisteLoquette <Baptisteloquette.entr@gmail.com>
2025-01-29 07:11:19 +0100

    Modified prompt for better performance on small models

--- src/rag/utils.py
+++ src/rag/utils.py
@@ -9,8 +9,7 @@ class CAG:
     def __init__(self, text_content, model_name="Qwen/Qwen2.5-0.5B-Instruct", 
                 question = "How can I subscribe and update my payment details?",
                 answer_instruction = """You are an helpfull assistant that gives give answers and help the user based on given context. You must ouput in French. Only output the answer, no other text.
-        If the user query is not related or outside of the given contexts, you must output "out of scope".
-        Make sure to answer in French."""
+                If the user's query is not related or outside of the given contexts, you must output "out of scope"."""
                 ):
         self.model_name = model_name
         self.text_content = text_content
@@ -22,7 +21,8 @@ class CAG:
         self.messages = [
             {"role": """system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant, designed for non technical people.
              You are an assistant, your task is to give answers and help the user based on the given context.
-             Your task is to output the right answer that respond to the user question. 
+             Your task is to output the accurate answer that respond to the user question.
+             If the user's query is not related or outside of the given contexts, you must output "out of scope".
              You must not ouput your reasoning, or any other unneeded text.
              """},
             {"role": "user", "content": self.prompt}
